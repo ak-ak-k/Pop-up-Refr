61a12f56599197f4169b3b69e0cf128eb0643e9c
ADITYA KIRTANE <adityakirtane27@gmail.com>
2022-06-27 16:06:27 +0530

    update

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
 function myFunction() {
-  var x = document.getElementById("myDIV");
-
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-    y.style.display = "none";
-  }
+  document.getElementById("myDIV").style.display = "block";
 }
 function closeForm() {
   document.getElementById("myDIV").style.display = "none";
